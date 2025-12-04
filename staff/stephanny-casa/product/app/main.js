18c46685e73b18d5353270fd80a8154e5389d8cc
Stephanny <casastephanny12@gmail.com>
2025-12-04 19:12:07 +0100

    add new folder called doc with an new file readne; add new view pet in homeview

--- staff/stephanny-casa/product/app/main.js
+++ staff/stephanny-casa/product/app/main.js
@@ -335,9 +335,22 @@ homeSubtitle.textContent = 'Welcome Home!'
 homeSubtitle.className = 'italic text-2xl my-4'
 homeView.appendChild(homeSubtitle)
 
+const homeAddPetButton = document.createElement('button')
+homeAddPetButton.textContent = '+ Pet'
+homeAddPetButton.type = 'button'
+homeAddPetButton.className = 'border-3 rounded-sm border-solid border-orange-500 my-4'
+homeView.appendChild(homeAddPetButton)
+
+homeAddPetButton.addEventListener('click', function(event){
+    event.preventDefault
+
+    homeView.style.display = 'none'
+    addPetView.style.display = ''
+})
+
 const logoutButton = document.createElement('button') // boton de salida al estar en la home view al logearte
 logoutButton.textContent = 'Logout' // agregamos texto que pulsaremos
-logoutButton.className = 'border-3 rounded-sm border-solid border-orange-500'
+logoutButton.className = 'border-3 rounded-sm border-solid border-orange-500 flex flex-end my-100'
 homeView.appendChild(logoutButton) // añadimos en la homeview el boton 
 
 logoutButton.addEventListener('click', function (event) {   //hacemos la funcion de event listener que al clickear salgamos de la homeview y nos redirigamos a la pagina de login
@@ -348,3 +361,103 @@ logoutButton.addEventListener('click', function (event) {   //hacemos la funcion
 })
 
 document.body.appendChild(homeView) // añadimos al document body para que sea visible.
+
+// add pet
+
+const addPetView = document.createElement('div')
+addPetView.style.display = 'none'
+
+const addPetTitle = document.createElement('h1')
+addPetTitle.textContent = 'MyPet'
+addPetTitle.className = 'font-bold italic text-4xl'
+addPetView.appendChild(addPetTitle)
+
+const addPetTopPanel = document.createElement('div')
+addPetTopPanel.className = 'flex justify-between'
+addPetView.appendChild(addPetTopPanel)
+
+const addPetSubtitle = document.createElement('h2')
+addPetSubtitle.textContent = 'Add Pet'
+addPetSubtitle.className = 'text-xl italic my-4'
+addPetTopPanel.appendChild(addPetSubtitle)
+
+const addPetBackLink = document.createElement('a')
+addPetBackLink.textContent = '< Back'
+addPetBackLink.href = ''
+addPetBackLink.className = 'bg-orange-300 rounded-sm border-2 border-black p-1 my-6 self-end'
+addPetTopPanel.appendChild(addPetBackLink)
+
+addPetBackLink.addEventListener('click', function(event){
+    event.preventDefault()
+
+    addPetView.style.display = 'none'
+    homeView.style.display = ''
+})
+
+const addPetForm = document.createElement('form')
+addPetForm.className = 'flex flex-col'
+
+const addPetNameLabel = document.createElement('label')
+addPetNameLabel.textContent = 'Name'
+addPetNameLabel.htmlFor = 'name'
+addPetForm.appendChild(addPetNameLabel)
+const addPetNameInput = document.createElement('input')
+addPetNameInput.id = 'name'
+addPetNameInput.type = 'text'
+addPetNameInput.className = 'border-2 boder-solid border-black rounded-lg p-1'
+addPetForm.appendChild(addPetNameInput)
+
+const addPetBirthdateLabel = document.createElement('label')
+addPetBirthdateLabel.textContent = 'Date of Birth'
+addPetBirthdateLabel.htmlFor = 'date'
+addPetForm.appendChild(addPetBirthdateLabel)
+
+const addPetBirthdateInput = document.createElement('input')
+addPetBirthdateInput.id = 'date'
+addPetBirthdateInput.type = 'date'
+addPetBirthdateInput.className = 'border-2 boder-solid border-black rounded-lg p-1'
+addPetForm.appendChild(addPetBirthdateInput)
+
+const addPetWeightLabel = document.createElement('label')
+addPetWeightLabel.textContent = 'Weight (kg)'
+addPetWeightLabel.htmlFor = 'weight'
+addPetForm.appendChild(addPetWeightLabel)
+
+const addPetWeightInput = document.createElement('input')
+addPetWeightInput.id = 'weight'
+addPetWeightInput.type = 'number'
+addPetWeightInput.className = 'border-2 boder-solid border-black rounded-lg p-1'
+addPetForm.appendChild(addPetWeightInput)
+
+const addPetImageLabel = document.createElement('label')
+addPetImageLabel.textContent = 'Image'
+addPetImageLabel.htmlFor = 'image'
+addPetForm.appendChild(addPetImageLabel)
+
+const addPetImageInput = document.createElement('input')
+addPetImageInput.id = 'image'
+addPetImageInput.type = 'url'
+addPetImageInput.className = 'border-2 boder-solid border-black rounded-lg p-1'
+addPetForm.appendChild(addPetImageInput)
+
+const addPetSubmitButton = document.createElement('button')
+addPetSubmitButton.textContent = 'Add Pet'
+addPetSubmitButton.type = 'submit'
+addPetSubmitButton.className = 'bg-orange-300 rounded-sm border-2 border-black p-1 my-6 self-center'
+addPetForm.appendChild(addPetSubmitButton)
+
+addPetView.appendChild(addPetForm)
+
+addPetForm.addEventListener('submit', function(event){
+    event.preventDefault()
+
+    const name = addPetNameInput
+    const birthdate = addPetBirthdateInput
+    const weight = addPetWeightInput
+    const image = addPetImageInput
+
+    console.log(name, birthdate, weight, image)
+
+})
+
+document.body.appendChild(addPetView)
---
+++ staff/stephanny-casa/product/doc/README.md
@@ -0,0 +1,46 @@
+# MyPet
+
+## Intro
+
+An for pet owners register their pets and their medical information, to have control on their health history. 
+
+![scary dog](https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExaDBhemsydm1sd2dnbmpwM3Vhb3N0am1uejhmcWVnc3ZraGg3bG51byZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gKHGnB1ml0moQdjhEJ/giphy.gif)
+
+## Functional desccription
+
+### Use cases 
+
+User
+- register
+- login
+- update credentials (username, password)
+- update profile (name, email, city, role, licensing number)
+
+Owner (User)
+
+- add pet
+- remove pet
+- list pets
+- add comment log for pet
+- remove comment log for pet
+- modify comment log for pet
+- list logs for pet 
+- filter veterinaries
+- assign veterinary for pet 
+- unassign veterinary for pet
+
+Veterinary (User)
+
+- list assigned pets
+- add action log for pet
+- remove action log for pet 
+- modify action log for pet
+- list logs for pet
+
+# UI/UX design 
+
+[Figma] (https://www.figma.com/design/s7ClxbZpTL1ymEfOiBkUDN/MyPet?node-id=0-1&p=f&t=NOpKqrFrG8cWxXP5-0)
+
+## Tecnhical
+
+// TODO
