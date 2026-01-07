d579e89306c229a64a57662bcaa3bb0e2a7ba816
Stephanny <casastephanny12@gmail.com>
2026-01-07 19:23:33 +0100

    separate wall and calculate in parts, data, logic and main

--- staff/stephanny-casa/playground/calculator/main.jsx
+++ staff/stephanny-casa/playground/calculator/main.jsx
@@ -4,301 +4,160 @@ root.render(<App />)
 const useState = React.useState
 
 function App() {
-
     const displayState = useState('0')
     const displayValue = displayState[0]
     const setDisplayValue = displayState[1]
 
-    //function oneClicked(){
-    //  console.log('one clicked')
-    //}
+    const handleOneClicked = () => {
+        logic.selectOne()
 
-    //functions click
-
-    //Back Button
-
-    const handleDeleteClicked = () => {
-
-        if (displayValue.length <= 1) {
-            setDisplayValue('0')
-        }
-
-        else {
-            setDisplayValue(displayValue.slice(0, -1))
-        }
+        setDisplayValue(logic.getCurrentValue())
     }
 
-    //Coma (,) Button
+    const handleTwoClicked = () => {
+        logic.selectTwo()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
+
+    const handleThreeClicked = () => {
+        logic.selectThree()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
+
+    const handleFourClicked = () => {
+        logic.selectFour()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
+
+    const handleFiveClicked = () => {
+        logic.selectFive()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
+
+    const handleSixClicked = () => {
+        logic.selectSix()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
+
+    const handleSevenClicked = () => {
+        logic.selectSeven()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
+
+    const handleEightClicked = () => {
+        logic.selectEight()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
+
+    const handleNineClicked = () => {
+        logic.selectNine()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
+
+    const handleZeroClicked = () => {
+        logic.selectZero()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
+
+    const handleAllClearClicked = () => {
+        logic.clearAll()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
+
+    const handleDivideClicked = () => {
+        logic.selectDivide()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
+
+    const handleMultiplyClicked = () => {
+        logic.selectMultiply()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
+
+    const handleSubtractClicked = () => {
+        logic.selectSubtract()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
+
+    const handleAddClicked = () => {
+        logic.selectAdd()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
+
+    const handleResultClicked = () => {
+        logic.calculate()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
+
+    const handleBackspaceClicked = () => {
+        logic.selectBackspace()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
 
     const handleCommaClicked = () => {
+        logic.selectComma()
 
-        const lastCharacter = displayValue.at(-1)
-
-        if (lastCharacter === ',') return
-
-        const lastIndexOfDivide = displayValue.lastIndexOf('÷')
-        const lastIndexOfMultiply = displayValue.lastIndexOf('×')
-        const lastIndexOfSubtract = displayValue.lastIndexOf('-')
-        const lastIndexOfAdd = displayValue.lastIndexOf('+')
-
-        const lastIndexOfOperation = Math.max(lastIndexOfDivide, lastIndexOfMultiply, lastIndexOfSubtract, lastIndexOfAdd)
-
-        const lastIndex = displayValue.length - 1
-
-        let newValue
-
-        if (lastIndexOfOperation === lastIndex)
-            newValue = displayValue + '0,'
-        else if (lastIndexOfOperation === -1) {
-            if (displayValue.includes(',')) return
-            newValue = displayValue + ','
-        } else {
-            const lastOperand = displayValue.slice(lastIndexOfOperation + 1)
-
-            if (lastOperand.includes(',')) return
-
-            newValue = displayValue + ','
-        }
-        setDisplayValue(newValue)
+        setDisplayValue(logic.getCurrentValue())
     }
 
     const handleChangeSignClicked = () => {
-        /*
-        const lastIndexOfDivide = displayValue.lastIndexOf('÷')
-        const lastIndexOfMultiply = displayValue.lastIndexOf('×')
-        const lastIndexOfSubtract = displayValue.lastIndexOf('-')
-        const lastIndexOfAdd = displayValue.lastIndexOf('+')
-
-        const lastIndexOfOperation = Math.max(lastIndexOfDivide, lastIndexOfMultiply, lastIndexOfSubtract, lastIndexOfAdd)
-
-        let newValue
-
-        if (lastIndexOfOperation === -1) {
-
-            if (displayValue === '0') return
-
-            if (!displayValue.includes('(')) {
-                newValue = '(-' + displayValue + ')'
-            } else {
-                const operand = displayValue.slice(2, displayValue.length - 1)
-
-                newValue = operand
-            }
-        }
-        setDisplayValue(newValue) 
-        */
-
-        const operands = []
-        const operators = []
-
-        let operand = ''
-
-        for (let i = 0; i < displayValue.length; i++) {
-
-            const char = displayValue[i]
-            const prevChar = displayValue[i - 1]
-
-            if (char !== '-' && prevchar !== '(' || char === '+' || char === '÷' || char === 'x') {
-                operands.push(operand)
-                operators.push(char)
-                operand = ''
-            } else {
-                operand += char
-
-                if (i === displayValue.length - 1)
-                    operands.push(operand)
-            }
-
-        }
-        if (operands.length === operators.length) return
-
-        let lastOperand = operands.at(-1)
-
-        if (lastOperand === '0') return
-
-        if (lastOperand.includes('('))
-            lastOperand = lastOperand.slice(2, -1)
-        else
-            lastOperand = '(-' + lastOperand + ')'
-
-        operands[operands.length - 1] = lastOperand
-
-        let newValue = ''
-
-        for (let i = 0; i < operands.length; i++) {
-            const operand = operands[i]
-
-            newValue += operand
-
-            const operator = operators[i]
-
-            if (operator)
-                newValue += operator
-        }
-
-        setDisplayValue(newValue)
-
-}
-
-
-
-//AC button
-
-const handleAllClearClicked = () => setDisplayValue('0')
-
-// ÷ button
-
-const handleDivideClicked = () => setDisplayValue(displayValue + '÷')
-
-// × button
-
-const handleMultiplyClicked = () => setDisplayValue(displayValue + '×')
-
-// - button
-
-const handleSubtractClicked = () => setDisplayValue(displayValue + '-')
-
-// + button
-
-const handleAddClicked = () => setDisplayValue(displayValue + '+')
-
-// = button
-
-const handleResultClicked = () => {
-    const operation = displayValue.replaceAll('÷', '/').replaceAll('×', '*')
-
-    const result = eval(operation)
-
-    setDisplayValue(String(result))
-}
-
-
-//numbers click 
-
-const handleZeroClicked = () => {
-    if (displayValue != '0')
-        setDisplayValue(displayValue + '0')
-}
-
-const handleOneClicked = () => {
-
-    if (displayValue === '0')
-        setDisplayValue('1')
-
-    else
-        setDisplayValue(displayValue + '1')
-}
-
-const handleTwoClicked = () => {
-
-    if (displayValue === '0')
-        setDisplayValue('2')
-
-    else
-        setDisplayValue(displayValue + '2')
-}
-
-const handleThreeClicked = () => {
-
-    if (displayValue === '0')
-        setDisplayValue('3')
-
-    else
-        setDisplayValue(displayValue + '3')
-}
-
-const handleFourClicked = () => {
-
-    if (displayValue === '0')
-        setDisplayValue('4')
-
-    else
-        setDisplayValue(displayValue + '4')
-}
-
-const handleFiveClicked = () => {
-
-    if (displayValue === '0')
-        setDisplayValue('5')
-
-    else
-        setDisplayValue(displayValue + '5')
-}
-
-const handleSixClicked = () => {
-
-    if (displayValue === '0')
-        setDisplayValue('6')
-
-    else
-        setDisplayValue(displayValue + '6')
-}
-
-const handleSevenClicked = () => {
-
-    if (displayValue === '0')
-        setDisplayValue('7')
-
-    else
-        setDisplayValue(displayValue + '7')
-}
-
-const handleEightClicked = () => {
-
-    if (displayValue === '0')
-        setDisplayValue('8')
-
-    else
-        setDisplayValue(displayValue + '8')
-}
-
-const handleNineClicked = () => {
-
-    if (displayValue === '0')
-        setDisplayValue('9')
-
-    else
-        setDisplayValue(displayValue + '9')
-}
-
-console.log('App -> render')
-
-return <div className="border-2 m-2 p-2 rounded-4xl bg-gray-800 text-white">
-    <div className="flex justify-end px-4 text-3xl">{displayValue}</div>
-
-    <div className="p-2 flex flex-col gap-2">
-        <div className="flex justify-between">
-            <div className="bg-gray-400 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleDeleteClicked}>⌫</div>
-            <div className="bg-gray-400 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleAllClearClicked}>AC</div>
-            <div className="bg-gray-400 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer">%</div>
-            <div className="bg-orange-400 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleDivideClicked}>÷</div>
+        logic.changeSign()
+
+        setDisplayValue(logic.getCurrentValue())
+    }
+
+    console.log('App -> render')
+
+    return <div className="border-2 m-2 p-2 rounded-2xl bg-gray-800 text-white">
+        <div className="flex justify-end px-4 text-3xl">{displayValue}</div>
+
+        <div className="p-2 flex flex-col gap-2">
+            <div className="flex justify-between">
+                <div className="bg-gray-400 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleBackspaceClicked}>⌫</div>
+                <div className="bg-gray-400 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleAllClearClicked}>AC</div>
+                <div className="bg-gray-400 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer">%</div>
+                <div className="bg-orange-400 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleDivideClicked}>÷</div>
+            </div>
+            <div className="flex justify-between">
+                <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleSevenClicked}>7</div>
+                <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleEightClicked}>8</div>
+                <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleNineClicked}>9</div>
+                <div className="bg-orange-400 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleMultiplyClicked}>×</div>
+            </div>
+            <div className="flex justify-between">
+                <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleFourClicked}>4</div>
+                <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleFiveClicked}>5</div>
+                <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleSixClicked}>6</div>
+                <div className="bg-orange-400 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleSubtractClicked}>-</div>
+            </div>
+            <div className="flex justify-between">
+                <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleOneClicked}>1</div>
+                <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleTwoClicked}>2</div>
+                <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleThreeClicked}>3</div>
+                <div className="bg-orange-400 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleAddClicked}>+</div>
+            </div>
+            <div className="flex justify-between">
+                <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleChangeSignClicked}>+/-</div>
+                <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleZeroClicked}>0</div>
+                <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleCommaClicked}>,</div>
+                <div className="bg-orange-400 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleResultClicked}>=</div>
+            </div>
         </div>
-        <div className="flex justify-between">
-            <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleSevenClicked}>7</div>
-            <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleEightClicked} >8</div>
-            <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleNineClicked}>9</div>
-            <div className="bg-orange-400 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleMultiplyClicked}>×</div>
-        </div>
-        <div className="flex justify-between">
-            <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleFourClicked}>4</div>
-            <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleFiveClicked}>5</div>
-            <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleSixClicked}>6</div>
-            <div className="bg-orange-400 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleSubtractClicked}>-</div>
-        </div>
-        <div className="flex justify-between">
-            <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleOneClicked}>1</div>
-            <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleTwoClicked}>2</div>
-            <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleThreeClicked}>3</div>
-            <div className="bg-orange-400 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleAddClicked}>+</div>
-        </div>
-        <div className="flex justify-between">
-            <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleChangeSignClicked}>+⁄-</div>
-            <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleZeroClicked}>0</div>
-            <div className="bg-gray-600 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleCommaClicked}>,</div>
-            <div className="bg-orange-400 p-2 rounded-full w-10 h-10 flex justify-center items-center cursor-pointer" onClick={handleResultClicked}>=</div>
-        </div>
-
-
     </div>
-</div>
 }
--- staff/stephanny-casa/playground/wall/main.jsx
+++ staff/stephanny-casa/playground/wall/main.jsx
@@ -4,36 +4,74 @@ root.render(<App />)
 const useState = React.useState
 
 function App() {
+    console.log('App -> call')
 
-    const messagesState = useState(['Hello, world! (Peter, 2025-12-19)'])
+    const messagesState = useState([])
     const messages = messagesState[0]
     const setMessages = messagesState[1]
 
     const handleMessageSubmit = event => {
         event.preventDefault()
 
-        console.log('message sent')
+        const form = event.target
+
+        const name = form.name.value
+        const message = form.message.value
+
+        try {
+            logic.addMessage(name, message)
+
+            form.reset()
+
+            const messages = logic.getAllMessages()
+
+            const newMessages = []
+
+            for (let i = 0; i < messages.length; i++) {
+                const message = messages[i]
+
+                newMessages.push(message)
+            } 
+
+            setMessages(newMessages)
+        } catch(error) {
+            console.error(error)
+        }
     }
 
-    return <div className= 'flex flex-col gap-2 p-2'>
-        <h1 className="text-3xl">Wall</h1>
+    console.log('App -> render')
 
-        <ul  className="p-2">
-            <li>
-                <p>{messages[0]}</p>
-            </li>
+    const listItems = []
+
+    for (let i = 0; i < messages.length; i++) {
+        const message = messages[i]
+
+        const messageString = message.text + '(' + message.author + ', ' + message.date + ')'
+
+        const listItem = <li>
+            <p>{messageString}</p>
+        </li>
+
+        listItems.push(listItem)
+    }
+
+    return <div className="flex flex-col gap-2 p-2">
+        <h1 className="text-3xl cursor-pointer">Wall</h1>
+
+        <ul className="p-2">
+            {listItems}
         </ul>
 
-       <form className="flex flex-col border p-2" onSubmit={handleMessageSubmit}>
-        <h2>Leave your message on the Wall</h2>
+        <form className="flex flex-col gap-2 border p-2" onSubmit={handleMessageSubmit}>
+            <h2>Leave your message on the wall!</h2>
 
-        <label htmlFor="message">Message</label>
-        <input className="border" type="text" id="message"></input>
+            <label htmlFor="message">Message</label>
+            <input className="border" type="text" id="message" placeholder="message" />
 
-        <label htmlFor="name">Name</label>
-        <input className="border" type="text" id="name"></input>
+            <label htmlFor="name">Name</label>
+            <input className="border" type="text" id="name" placeholder="name" />
 
-        <button className="border" type="submit">Send</button>
-       </form>
+            <button className="border bg-black text-white cursor-pointer" type="submit">Send</button>
+        </form>
     </div>
 }
