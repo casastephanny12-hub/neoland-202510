8919cbcca0fec83bb0a5926e80f08cf7d7370551
Stephanny <casastephanny12@gmail.com>
2026-02-02 22:11:32 +0100

    add people endpoint for people in api

--- staff/stephanny-casa/product/api/index.js
+++ staff/stephanny-casa/product/api/index.js
@@ -4,8 +4,20 @@ const express = require('express')
 
 const api = express()
 
-api.get('/hello', (req, res) => {
-    res.send('Hello!')
+const people = [
+    { id: 'person-0', name: 'Carla', age: 34 },
+    { id: 'person-1', name: 'Nohely', age: 26 },
+    { id: 'person-2', name: 'Cindy', age: 29 },
+]
+
+api.get('/', (req, res) => res.json({ hello: 'world' }))
+
+api.get('/people', (req, res) => {
+    const personId = req.query.id 
+
+    const person = people.find(person => person.id === personId )
+
+    res.json(person)
 })
 
 api.listen(8080, () => console.log('API listening on port 8080'))
